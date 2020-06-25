e33afc954952a8389ed834506714cb010926070b
Ryan Gurnick <ryangurnick@gmail.com>
2020-06-25 12:21:07 -0700

    Create SettingCategory.php

---
+++ app/SettingCategory.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SettingCategory extends Model
+{
+    // settings
+    public function settings()
+    {
+        return $this->hasMany(Setting::class);
+    }
+}
