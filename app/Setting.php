f9aba246173dedbb3793e8bd8a1ab9cc4e51cc6b
Ryan Gurnick <ryangurnick@gmail.com>
2020-06-25 11:54:13 -0700

    Store payload as array
    We want to store and treat the payload column as an array, this will allow laravel to do some tricks for us via the ORM.

--- app/Setting.php
+++ app/Setting.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Setting extends Model
 {
+    protected $casts = [
+        'payload' => 'array'
+    ];
+
     // category
     public function category()
     {
