0ab248ff956a43f206ad7e56c4e979ae8fda5918
Ryan Gurnick <ryangurnick@gmail.com>
2020-06-25 21:56:51 -0700

    Inserting empty factories
    There is not a need to define these right now!

---
+++ database/factories/SettingCategoryFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\SettingCategory;
+use Faker\Generator as Faker;
+
+$factory->define(SettingCategory::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
