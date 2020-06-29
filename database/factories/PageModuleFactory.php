731d0ae196adfa69128dffe974c5dd6e8e240795
Ryan Gurnick <ryangurnick@gmail.com>
2020-06-28 21:08:29 -0700

    Adding in empty factory and seed for module.

---
+++ database/factories/PageModuleFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\PageModule;
+use Faker\Generator as Faker;
+
+$factory->define(PageModule::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
