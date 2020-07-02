0937067699476f44ad9be97268802d28f8fd4ff3
Ryan Gurnick <ryangurnick@gmail.com>
2020-07-01 23:14:07 -0700

    Working on modal for basics
    This first one will take the longest to figure out. Then I will be able to knock these out quickly. It is really annoying right now adding in all the subcomponents that make this an ok ui to use.

--- resources/js/components/Card/BasicsCard.js
+++ resources/js/components/Card/BasicsCard.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
-import {Label, Container, Grid, Card, Icon, Button} from "semantic-ui-react";
+import {Label, Container, Grid, Card, Icon, Button, Modal, Form} from "semantic-ui-react";
 import PropTypes from 'prop-types';
 
 class BasicsCard extends Component {
+    state = {open: false}
+
+    show = () => () => this.setState({open: true})
+    close = () => this.setState({open: false})
+
     render() {
+        const { open } = this.state;
         const labels = this.props.items.map((item, key) =>
             <Grid.Column>
                 <Label as={'a'} image color={item.color} key={item.color}>
@@ -13,26 +19,40 @@ class BasicsCard extends Component {
             </Grid.Column>
         );
         return (
-            <Card fluid>
-                <Card.Content>
-                    <Card.Header>
-                        <Icon name={'user circle'} />
-                        {this.props.name}
+            <div>
+                <Card fluid>
+                    <Card.Content>
+                        <Card.Header>
+                            <Icon name={'user circle'} />
+                            {this.props.name}
 
-                        <Button animated={true} size={'mini'} className={'compact right floated'}>
-                            <Button.Content visible>Edit</Button.Content>
-                            <Button.Content hidden>
-                                <Icon name={'edit outline'} />
-                            </Button.Content>
-                        </Button>
-                    </Card.Header>
-                    <Card.Description>
-                        <Grid columns={2}>
-                            {labels}
-                        </Grid>
-                    </Card.Description>
-                </Card.Content>
-            </Card>
+                            <Button animated={true} size={'mini'} className={'compact right floated'} onClick={this.show()}>
+                                <Button.Content visible>Edit</Button.Content>
+                                <Button.Content hidden>
+                                    <Icon name={'edit outline'} />
+                                </Button.Content>
+                            </Button>
+                        </Card.Header>
+                        <Card.Description>
+                            <Grid columns={2}>
+                                {labels}
+                            </Grid>
+                        </Card.Description>
+                    </Card.Content>
+                </Card>
+                <Modal as={'form'} size={'tiny'} open={open} onClose={this.close} style={{position: 'static', height: 'auto'}} className={'form'}>
+                    <Modal.Header>Edit Basics</Modal.Header>
+                    <Modal.Content>
+                        <Form.Group>
+                            <Form.Input label='Text' placeholder={'Text'} type='text' width={16} />
+                        </Form.Group>
+                    </Modal.Content>
+                    <Modal.Actions>
+                        <Button color={'red'} onClick={this.close}>Cancel</Button>
+                        <Button color={'green'}>Save</Button>
+                    </Modal.Actions>
+                </Modal>
+            </div>
         );
     }
 }
