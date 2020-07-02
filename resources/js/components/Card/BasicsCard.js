1bbfa237ee47fd3757fdfc67d6797bfbb70911b3
Ryan Gurnick <ryangurnick@gmail.com>
2020-07-01 23:28:32 -0700

    Adding initial accordion to modal
    This will add better support for accordions within the basics modal as designed. This adds in some styling so that its not just a simple modal! This will need to be refined for the colors and icons inputs.

--- resources/js/components/Card/BasicsCard.js
+++ resources/js/components/Card/BasicsCard.js
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
-import {Label, Container, Grid, Card, Icon, Button, Modal, Form} from "semantic-ui-react";
+import {Label, Accordion, Grid, Card, Icon, Button, Modal, Form} from "semantic-ui-react";
 import PropTypes from 'prop-types';
 
 class BasicsCard extends Component {
-    state = {open: false}
+    state = {open: false, activeIndex: 0}
 
     show = () => () => this.setState({open: true})
     close = () => this.setState({open: false})
+    handleClick = (e, titleProps) => {
+        const { index } = titleProps
+        const { activeIndex } = this.state
+        const newIndex = activeIndex === index ? -1 : index
+
+        this.setState({ activeIndex: newIndex })
+    }
+
 
     render() {
-        const { open } = this.state;
+        const { open, activeIndex } = this.state;
         const labels = this.props.items.map((item, key) =>
             <Grid.Column>
                 <Label as={'a'} image color={item.color} key={item.color}>
@@ -46,6 +54,55 @@ class BasicsCard extends Component {
                         <Form.Group>
                             <Form.Input label='Text' placeholder={'Text'} type='text' width={16} />
                         </Form.Group>
+                        <Accordion styled>
+                            <Accordion.Title active={activeIndex === 0} index={0} onClick={this.handleClick}>
+                                <Icon name='dropdown' />
+                                What is a dog?
+                            </Accordion.Title>
+                            <Accordion.Content active={activeIndex === 0}>
+                                <Form.Group>
+                                    <Form.Input label='Text' placeholder={'Text'} type='text' width={16} />
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Input label='Color' placeholder={'Color'} type='text' width={16} />
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Input label='Icon' placeholder={'Icon'} type='text' width={16} />
+                                </Form.Group>
+                            </Accordion.Content>
+
+                            <Accordion.Title active={activeIndex === 1} index={1} onClick={this.handleClick}>
+                                <Icon name='dropdown' />
+                                What kinds of dogs are there?
+                            </Accordion.Title>
+                            <Accordion.Content active={activeIndex === 1}>
+                                <Form.Group>
+                                    <Form.Input label='Text' placeholder={'Text'} type='text' width={16} />
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Input label='Color' placeholder={'Color'} type='text' width={16} />
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Input label='Icon' placeholder={'Icon'} type='text' width={16} />
+                                </Form.Group>
+                            </Accordion.Content>
+
+                            <Accordion.Title active={activeIndex === 2} index={2} onClick={this.handleClick}>
+                                <Icon name='dropdown' />
+                                How do you acquire a dog?
+                            </Accordion.Title>
+                            <Accordion.Content active={activeIndex === 2}>
+                                <Form.Group>
+                                    <Form.Input label='Text' placeholder={'Text'} type='text' width={16} />
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Input label='Color' placeholder={'Color'} type='text' width={16} />
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Input label='Icon' placeholder={'Icon'} type='text' width={16} />
+                                </Form.Group>
+                            </Accordion.Content>
+                        </Accordion>
                     </Modal.Content>
                     <Modal.Actions>
                         <Button color={'red'} onClick={this.close}>Cancel</Button>
