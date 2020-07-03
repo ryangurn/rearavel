206e037c44c8b84779fdefd5503fd249b456e1cb
Ryan Gurnick <ryangurnick@gmail.com>
2020-07-02 23:24:17 -0700

    Adding in accordion for computer skills modal
    This will show the information within the modal that needs to be shown, what fun!

--- resources/js/components/Card/ComputerSkillsCard.js
+++ resources/js/components/Card/ComputerSkillsCard.js
@@ -5,46 +5,99 @@ import {
     Icon,
     Grid,
     List,
-    Button
+    Button,
+    Modal,
+    Form,
+    Accordion
 } from "semantic-ui-react";
 import PropTypes from 'prop-types';
+import ColorInput from "../Core/Input/ColorInput";
+import IconInput from "../Core/Input/IconInput";
 
 class ComputerSkillsCard extends Component {
+    state = {open: false, activeIndex: 0}
+
+    show = () => () => this.setState({open: true})
+    close = () => this.setState({open: false})
+    handleClick = (e, titleProps) => {
+        const { index } = titleProps
+        const { activeIndex } = this.state
+        const newIndex = activeIndex === index ? -1 : index
+
+        this.setState({ activeIndex: newIndex })
+    }
+
     render() {
+        const { open, activeIndex } = this.state;
+        const accordion = this.props.items.map((item, key) =>
+            <div>
+                <Accordion.Title active={activeIndex === key} index={key} onClick={this.handleClick}>
+                    <Icon name='dropdown' />
+                    {item.section}
+                </Accordion.Title>
+                <Accordion.Content active={activeIndex === key}>
+                    { item.texts.map((i, k) =>
+                        <Form.Group fluid>
+                            <Form.Input label='Text' placeholder={'Text'} type='text' value={i} action width={16}>
+                                <input />
+                                <Button color={'red'} type='button'>Remove</Button>
+                            </Form.Input>
+                        </Form.Group>
+                    )}
+                </Accordion.Content>
+            </div>
+        );
         return (
-            <Card fluid>
-                <Card.Content>
-                    <Card.Header>
-                        <Icon name={'desktop'} />
-                        {this.props.name}
-                        <Button animated={true} size={'mini'} className={'compact right floated'}>
-                            <Button.Content visible>Edit</Button.Content>
-                            <Button.Content hidden>
-                                <Icon name={'edit outline'} />
-                            </Button.Content>
-                        </Button>
-                    </Card.Header>
-                    <Card.Description>
-                        <Grid columns={2}>
-                            { this.props.items.map((innerArr, k) => (
-                                <Grid.Column key={k.toString()}>
-                                    <h4>{innerArr.section}</h4>
-                                    <List key={k}>
-                                        { innerArr.texts.map((item, key) =>
-                                            <List.Item key={key.toString()}>
-                                                <List.Icon name={'right triangle'} />
-                                                <List.Content>
-                                                    <List.Header>{item}</List.Header>
-                                                </List.Content>
-                                            </List.Item>
-                                        )}
-                                    </List>
-                                </Grid.Column>
-                            ))}
-                        </Grid>
-                    </Card.Description>
-                </Card.Content>
-            </Card>
+            <div style={{paddingTop: '14px'}}>
+                <Card fluid>
+                    <Card.Content>
+                        <Card.Header>
+                            <Icon name={'desktop'} />
+                            {this.props.name}
+                            <Button animated={true} size={'mini'} className={'compact right floated'} onClick={this.show()}>
+                                <Button.Content visible>Edit</Button.Content>
+                                <Button.Content hidden>
+                                    <Icon name={'edit outline'} />
+                                </Button.Content>
+                            </Button>
+                        </Card.Header>
+                        <Card.Description>
+                            <Grid columns={2}>
+                                { this.props.items.map((innerArr, k) => (
+                                    <Grid.Column key={k.toString()}>
+                                        <h4>{innerArr.section}</h4>
+                                        <List key={k}>
+                                            { innerArr.texts.map((item, key) =>
+                                                <List.Item key={key.toString()}>
+                                                    <List.Icon name={'right triangle'} />
+                                                    <List.Content>
+                                                        <List.Header>{item}</List.Header>
+                                                    </List.Content>
+                                                </List.Item>
+                                            )}
+                                        </List>
+                                    </Grid.Column>
+                                ))}
+                            </Grid>
+                        </Card.Description>
+                    </Card.Content>
+                </Card>
+                <Modal as={'form'} size={'tiny'} open={open} onClose={this.close} style={{position: 'static', height: 'auto'}} className={'form'}>
+                    <Modal.Header>Edit Computer Skills</Modal.Header>
+                    <Modal.Content scrolling>
+                        <Form.Group>
+                            <Form.Input label='Title' placeholder={'Title'} type='text' width={16} />
+                        </Form.Group>
+                        <Accordion styled>
+                            {accordion}
+                        </Accordion>
+                    </Modal.Content>
+                    <Modal.Actions>
+                        <Button color={'red'} onClick={this.close}>Cancel</Button>
+                        <Button color={'green'}>Save</Button>
+                    </Modal.Actions>
+                </Modal>
+            </div>
         );
     }
 }
